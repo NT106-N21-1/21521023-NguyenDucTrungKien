a037878a47555cb97df4d7eeffe9580e91948f15
Kiên Nguyễn Đức Trung <21521023@gm.uit.edu.vn>
2023-03-17 14:04:49 +0700

    lab1: Tinh tong 2 so nguyen(Bai 1)

--- Lab1/Form1.Designer.cs
+++ Lab1/Form1.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblHeader = new Label();
+            lblNum1 = new Label();
+            lblNum2 = new Label();
+            lblKetQua = new Label();
+            txtNum1 = new TextBox();
+            txtNum2 = new TextBox();
+            txtResult = new TextBox();
+            btnTinh = new Button();
+            SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            lblHeader.AutoSize = true;
+            lblHeader.Font = new Font("Segoe UI", 13F, FontStyle.Bold, GraphicsUnit.Point);
+            lblHeader.ForeColor = Color.Red;
+            lblHeader.Location = new Point(251, 9);
+            lblHeader.Name = "lblHeader";
+            lblHeader.Size = new Size(340, 36);
+            lblHeader.TabIndex = 0;
+            lblHeader.Text = "TÍNH TỔNG 2 SỐ NGUYÊN";
+            lblHeader.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblNum1
+            // 
+            lblNum1.AutoSize = true;
+            lblNum1.Location = new Point(59, 97);
+            lblNum1.Name = "lblNum1";
+            lblNum1.Size = new Size(105, 25);
+            lblNum1.TabIndex = 1;
+            lblNum1.Text = "Số thứ nhất";
+            // 
+            // lblNum2
+            // 
+            lblNum2.AutoSize = true;
+            lblNum2.Location = new Point(59, 157);
+            lblNum2.Name = "lblNum2";
+            lblNum2.Size = new Size(93, 25);
+            lblNum2.TabIndex = 2;
+            lblNum2.Text = "Số thứ hai";
+            // 
+            // lblKetQua
+            // 
+            lblKetQua.AutoSize = true;
+            lblKetQua.Location = new Point(69, 264);
+            lblKetQua.Name = "lblKetQua";
+            lblKetQua.Size = new Size(72, 25);
+            lblKetQua.TabIndex = 3;
+            lblKetQua.Text = "Kết quả";
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(170, 97);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(304, 31);
+            txtNum1.TabIndex = 4;
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(170, 157);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(304, 31);
+            txtNum2.TabIndex = 5;
+            // 
+            // txtResult
+            // 
+            txtResult.Location = new Point(170, 264);
+            txtResult.Name = "txtResult";
+            txtResult.ReadOnly = true;
+            txtResult.Size = new Size(304, 31);
+            txtResult.TabIndex = 6;
+            // 
+            // btnTinh
+            // 
+            btnTinh.Location = new Point(362, 194);
+            btnTinh.Name = "btnTinh";
+            btnTinh.Size = new Size(112, 64);
+            btnTinh.TabIndex = 7;
+            btnTinh.Text = "Tính";
+            btnTinh.UseVisualStyleBackColor = true;
+            btnTinh.Click += btnTinh_Click;
+            // 
+            // Form1
+            // 
+            AcceptButton = btnTinh;
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(877, 429);
+            Controls.Add(btnTinh);
+            Controls.Add(txtResult);
+            Controls.Add(txtNum2);
+            Controls.Add(txtNum1);
+            Controls.Add(lblKetQua);
+            Controls.Add(lblNum2);
+            Controls.Add(lblNum1);
+            Controls.Add(lblHeader);
+            Name = "Form1";
+            Text = "Lab1_Bai1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblHeader;
+        private Label lblNum1;
+        private Label lblNum2;
+        private Label lblKetQua;
+        private TextBox txtNum1;
+        private TextBox txtNum2;
+        private TextBox txtResult;
+        private Button btnTinh;
     }
 }
--- Lab1/Form1.cs
+++ Lab1/Form1.cs
@@ -1,10 +1,44 @@
-namespace Lab1
+﻿namespace Lab1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            int i = 0, j = 0, count1 = 0, count2 = 0;
+            int num1, num2;
+            long result;
+            while (i < txtNum1.Text.Length)
+            {
+                if (txtNum1.Text[i] < 48 || txtNum1.Text[i] > 57)
+                {
+                    count1++;
+                }
+                i++;
+            }
+            while (j < txtNum2.Text.Length)
+            {
+                if (txtNum2.Text[j] < 48 || txtNum2.Text[j] > 57)
+                {
+                    count2++;
+                }
+                j++;
+            }
+            if (count1 != 0 || count2 != 0)
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                num1 = Int32.Parse(txtNum1.Text);
+                num2 = Int32.Parse(txtNum2.Text);
+                result = num1 + num2;
+                txtResult.Text = result.ToString();
+            }
         }
     }
 }
