bbc606c851bef8459c7872679208da058a8980c2
Kiên Nguyễn Đức Trung <21521023@ms.uit.edu.vn>
2023-03-25 16:21:39 +0700

    lab1: Tinh tien xang(Bai 4)

--- Lab1/Bai4.Designer.cs
+++ Lab1/Bai4.Designer.cs
@@ -28,12 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Bai4";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Bai4));
+            lblXe = new Label();
+            lblXang = new Label();
+            lblKhoangCach = new Label();
+            lblTien = new Label();
+            cbXe = new ComboBox();
+            cbXang = new ComboBox();
+            btnTinh = new Button();
+            txtKhoangCach = new TextBox();
+            txtTien = new TextBox();
+            lbl_Info_Xe = new Label();
+            lbl_Info_Xang = new Label();
+            lbl_Donvi_km = new Label();
+            lbl_Donvi_VND = new Label();
+            SuspendLayout();
+            // 
+            // lblXe
+            // 
+            lblXe.AutoSize = true;
+            lblXe.Location = new Point(107, 40);
+            lblXe.Name = "lblXe";
+            lblXe.Size = new Size(59, 20);
+            lblXe.TabIndex = 0;
+            lblXe.Text = "Loại xe:";
+            // 
+            // lblXang
+            // 
+            lblXang.AutoSize = true;
+            lblXang.Location = new Point(61, 87);
+            lblXang.Name = "lblXang";
+            lblXang.Size = new Size(105, 20);
+            lblXang.TabIndex = 1;
+            lblXang.Text = "Loại xăng dầu:";
+            // 
+            // lblKhoangCach
+            // 
+            lblKhoangCach.AutoSize = true;
+            lblKhoangCach.Location = new Point(58, 282);
+            lblKhoangCach.Name = "lblKhoangCach";
+            lblKhoangCach.Size = new Size(108, 20);
+            lblKhoangCach.TabIndex = 2;
+            lblKhoangCach.Text = "Số km đi được:";
+            // 
+            // lblTien
+            // 
+            lblTien.AutoSize = true;
+            lblTien.Location = new Point(59, 334);
+            lblTien.Name = "lblTien";
+            lblTien.Size = new Size(107, 20);
+            lblTien.TabIndex = 3;
+            lblTien.Text = "Số tiền cần trả:";
+            // 
+            // cbXe
+            // 
+            cbXe.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbXe.FormattingEnabled = true;
+            cbXe.Items.AddRange(new object[] { "Wave Alpha", "Sirius", "Vision", "Lead", "Winner", "AirBlade 150", "Xe tải 9 tấn" });
+            cbXe.Location = new Point(172, 37);
+            cbXe.Name = "cbXe";
+            cbXe.Size = new Size(151, 28);
+            cbXe.TabIndex = 4;
+            // 
+            // cbXang
+            // 
+            cbXang.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbXang.FormattingEnabled = true;
+            cbXang.Items.AddRange(new object[] { "Xăng RON 95-III", "Xăng E5 RON 92-II", "Dầu DO 0,05S-II" });
+            cbXang.Location = new Point(172, 84);
+            cbXang.Name = "cbXang";
+            cbXang.Size = new Size(151, 28);
+            cbXang.TabIndex = 5;
+            // 
+            // btnTinh
+            // 
+            btnTinh.BackColor = SystemColors.ActiveBorder;
+            btnTinh.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTinh.Location = new Point(75, 158);
+            btnTinh.Name = "btnTinh";
+            btnTinh.Size = new Size(282, 50);
+            btnTinh.TabIndex = 6;
+            btnTinh.Text = "Tính";
+            btnTinh.UseVisualStyleBackColor = false;
+            btnTinh.Click += btnTinh_Click;
+            // 
+            // txtKhoangCach
+            // 
+            txtKhoangCach.Location = new Point(172, 279);
+            txtKhoangCach.Name = "txtKhoangCach";
+            txtKhoangCach.ReadOnly = true;
+            txtKhoangCach.Size = new Size(151, 27);
+            txtKhoangCach.TabIndex = 7;
+            // 
+            // txtTien
+            // 
+            txtTien.Location = new Point(172, 331);
+            txtTien.Name = "txtTien";
+            txtTien.ReadOnly = true;
+            txtTien.Size = new Size(151, 27);
+            txtTien.TabIndex = 8;
+            // 
+            // lbl_Info_Xe
+            // 
+            lbl_Info_Xe.AutoSize = true;
+            lbl_Info_Xe.Location = new Point(449, 37);
+            lbl_Info_Xe.Name = "lbl_Info_Xe";
+            lbl_Info_Xe.Size = new Size(537, 160);
+            lbl_Info_Xe.TabIndex = 11;
+            lbl_Info_Xe.Text = resources.GetString("lbl_Info_Xe.Text");
+            // 
+            // lbl_Info_Xang
+            // 
+            lbl_Info_Xang.AutoSize = true;
+            lbl_Info_Xang.Location = new Point(450, 279);
+            lbl_Info_Xang.Name = "lbl_Info_Xang";
+            lbl_Info_Xang.Size = new Size(266, 80);
+            lbl_Info_Xang.TabIndex = 12;
+            lbl_Info_Xang.Text = "Thông tin tỉ giá xăng dầu:\r\n    - Xăng RON 95-III: 26,830 đồng/lít\r\n    - Xăng E5 RON 92-II: 26,070 đồng/lít\r\n    - Dầu DO 0,05S-II: 21,310 đồng/lít";
+            // 
+            // lbl_Donvi_km
+            // 
+            lbl_Donvi_km.AutoSize = true;
+            lbl_Donvi_km.Location = new Point(329, 282);
+            lbl_Donvi_km.Name = "lbl_Donvi_km";
+            lbl_Donvi_km.Size = new Size(39, 20);
+            lbl_Donvi_km.TabIndex = 13;
+            lbl_Donvi_km.Text = "(km)";
+            // 
+            // lbl_Donvi_VND
+            // 
+            lbl_Donvi_VND.AutoSize = true;
+            lbl_Donvi_VND.Location = new Point(329, 334);
+            lbl_Donvi_VND.Name = "lbl_Donvi_VND";
+            lbl_Donvi_VND.Size = new Size(50, 20);
+            lbl_Donvi_VND.TabIndex = 13;
+            lbl_Donvi_VND.Text = "(VNĐ)";
+            // 
+            // Bai4
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1012, 410);
+            Controls.Add(lbl_Donvi_VND);
+            Controls.Add(lbl_Donvi_km);
+            Controls.Add(lbl_Info_Xang);
+            Controls.Add(lbl_Info_Xe);
+            Controls.Add(txtTien);
+            Controls.Add(txtKhoangCach);
+            Controls.Add(btnTinh);
+            Controls.Add(cbXang);
+            Controls.Add(cbXe);
+            Controls.Add(lblTien);
+            Controls.Add(lblKhoangCach);
+            Controls.Add(lblXang);
+            Controls.Add(lblXe);
+            Name = "Bai4";
+            Text = "Bai4";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblXe;
+        private Label lblXang;
+        private Label lblKhoangCach;
+        private Label lblTien;
+        private ComboBox cbXe;
+        private ComboBox cbXang;
+        private Button btnTinh;
+        private TextBox txtKhoangCach;
+        private TextBox txtTien;
+        private Label lblXangInfo;
+        private Label label6;
+        private Label lbl_Info_Xe;
+        private Label lbl_Info_Xang;
+        private Label label8;
+        private Label lbl_Donvi_km;
+        private Label lbl_Donvi_VND;
     }
 }
--- Lab1/Bai4.cs
+++ Lab1/Bai4.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,96 @@ namespace Lab1
         public Bai4()
         {
             InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            if (cbXe.Text == "" || cbXang.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if ((cbXe.Text == "Wave Alpha" && cbXang.Text == "Dầu DO 0,05S-II") || (cbXe.Text == "Sirius" && cbXang.Text == "Dầu DO 0,05S-II") || (cbXe.Text == "Vision" && cbXang.Text == "Dầu DO 0,05S-II") || (cbXe.Text == "Lead" && cbXang.Text == "Dầu DO 0,05S-II") || (cbXe.Text == "Lead" && cbXang.Text == "Xăng E5 RON 92-II") || (cbXe.Text == "Winner" && cbXang.Text == "Dầu DO 0,05S-II") || (cbXe.Text == "Winner" && cbXang.Text == "Xăng E5 RON 92-II") || (cbXe.Text == "AirBlade 150" && cbXang.Text == "Dầu DO 0,05S-II") || (cbXe.Text == "AirBlade 150" && cbXang.Text == "Xăng E5 RON 92-II") || (cbXe.Text == "Xe tải 9 tấn" && cbXang.Text == "Xăng RON 95-III") || (cbXe.Text == "Xe tải 9 tấn" && cbXang.Text == "Xăng E5 RON 92-II"))
+            {
+                txtKhoangCach.Text = "";
+                txtTien.Text = "";
+                MessageBox.Show("Xe không sử dụng loại xăng/dầu này. Vui lòng chọn lại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                double gia = 0;
+                double succhua = 0;
+                double tieuton = 0;
+                double khoangcach = 0;
+                double tongtien = 0;
+                switch (cbXe.Text)
+                {
+                    case "Wave Alpha":
+                        {
+                            succhua = 3.7;
+                            tieuton = 1.6;
+                            break;
+                        }
+                    case "Sirius":
+                        {
+                            succhua = 3.8;
+                            tieuton = 1.99;
+                            break;
+                        }
+                    case "Vision":
+                        {
+                            succhua = 5.2;
+                            tieuton = 1.87;
+                            break;
+                        }
+                    case "Lead":
+                        {
+                            succhua = 6;
+                            tieuton = 2.02;
+                            break;
+                        }
+                    case "Winner":
+                        {
+                            succhua = 4.5;
+                            tieuton = 1.7;
+                            break;
+                        }
+
+                    case "AirBlade 150":
+                        {
+                            succhua = 4.4;
+                            tieuton = 2.17;
+                            break;
+                        }
+                    case "Xe tải 9 tấn":
+                        {
+                            succhua = 70;
+                            tieuton = 13;
+                            break;
+                        }
+                }
+                switch (cbXang.Text)
+                {
+                    case "Xăng RON 95-III":
+                        {
+                            gia = 26830;
+                            break;
+                        }
+                    case "Xăng E5 RON 92-II":
+                        {
+                            gia = 26070;
+                            break;
+                        }
+                    case "Dầu DO 0,05S-II":
+                        {
+                            gia = 21310;
+                            break;
+                        }
+                }
+                khoangcach = Math.Round((succhua * 100) / tieuton, 3);
+                tongtien = Math.Round(gia * succhua);
+                txtKhoangCach.Text = khoangcach.ToString();
+                txtTien.Text = string.Format(new CultureInfo("vi-VN"), "{0:#,##0}", tongtien);
+            }
         }
     }
 }
