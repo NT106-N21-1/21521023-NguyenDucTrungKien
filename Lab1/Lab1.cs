a42356ba75efc44fa81baddcd68687b39dceafc4
Kiên Nguyễn Đức Trung <21521023@ms.uit.edu.vn>
2023-03-30 14:17:54 +0700

    lab1: create mainform

--- Lab1/Lab1.cs
+++ Lab1/Lab1.cs
@@ -28,5 +28,35 @@ namespace Lab1
             Bai2 bai2 = new Bai2();
             bai2.ShowDialog();
         }
+
+        private void btnBai3_Click(object sender, EventArgs e)
+        {
+            Bai3 bai3 = new Bai3();
+            bai3.ShowDialog();
+        }
+
+        private void btnBai3_1_Click(object sender, EventArgs e)
+        {
+            Bai3_1 bai3_1 = new Bai3_1();
+            bai3_1.ShowDialog();
+        }
+
+        private void btnBai4_Click(object sender, EventArgs e)
+        {
+            Bai4 bai4 = new Bai4();
+            bai4.ShowDialog();
+        }
+
+        private void btnBai5_Click(object sender, EventArgs e)
+        {
+            Bai5 bai5 = new Bai5();
+            bai5.ShowDialog();
+        }
+
+        private void btnBai7_Click(object sender, EventArgs e)
+        {
+            Bai7 bai7 = new Bai7();
+            bai7.ShowDialog();
+        }
     }
 }
