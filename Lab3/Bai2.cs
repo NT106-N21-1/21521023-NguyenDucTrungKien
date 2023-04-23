69bbda5488ca9603438caf57748882392706388f
Kiên Nguyễn Đức Trung <21521023@ms.uit.edu.vn>
2023-04-23 08:42:41 +0700

    Lab3: Bài 2

---
+++ Lab3/Bai2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Lab3
+{
+    public partial class Bai2 : Form
+    {
+        public Bai2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread serverThread = new Thread(new ThreadStart(StartUnsafeThread));
+            serverThread.Start();
+            btnListen.Enabled = false;
+        }
+        void StartUnsafeThread()
+        {
+            int receivedBytes = 0;
+            byte[] recv = new byte[1];
+            Socket clientSocket;
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipepServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            listenerSocket.Bind(ipepServer);
+            listenerSocket.Listen(-1);
+            clientSocket = listenerSocket.Accept();
+            lv_Show.Items.Add(new ListViewItem("New client connected!"));
+            while (clientSocket.Connected)
+            {
+                string text = "";
+                do
+                {
+                    receivedBytes = clientSocket.Receive(recv);
+                    text += Encoding.UTF8.GetString(recv);
+                }
+                while (text[text.Length - 1] != '\n' && clientSocket.Connected);
+                lv_Show.Items.Add(new ListViewItem(text));
+            }
+            listenerSocket.Close();
+        }
+    }
+}
